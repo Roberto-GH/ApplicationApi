409b9382b3183f6281f8b83ecf02de830997cd4a
Roberto Londoño <roberthdrums@gmail.com>
2025-09-22 23:52:23 -0500

    fix

--- infrastructure/driven-adapters/rest-consumer/src/main/java/co/com/pragma/consumer/config/AuthSecretModel.java
+++ infrastructure/driven-adapters/rest-consumer/src/main/java/co/com/pragma/consumer/config/AuthSecretModel.java
@@ -1,5 +1,6 @@
 package co.com.pragma.consumer.config;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,9 @@ import lombok.Setter;
 @Setter
 public class AuthSecretModel {
 
+  @SerializedName("ADMIN_EMAIL")
   private String email;
+  @SerializedName("ADMIN_PASSWORD")
   private String password;
 
 }
